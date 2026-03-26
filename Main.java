234224f02cd7e49f7c632135dbd2c32638636d5c
vk1700 <Varunkarunakaran10@gmail.com>
2026-03-26 14:01:30 +0530

    UC3: Display Hello with name or default World

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        // ===== UC1 =====
-        System.out.println("Hello World");
+        // ===== UC3 =====
+        String name;
 
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello " + name);
     }
 }
